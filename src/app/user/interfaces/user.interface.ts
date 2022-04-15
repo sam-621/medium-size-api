7b8ddc72fee609bf478713f14deea371b060c1bb
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-04-15 12:34:55 -0600

    add types for user register and user login

--- src/app/user/interfaces/user.interface.ts
+++ src/app/user/interfaces/user.interface.ts
@@ -8,3 +8,6 @@ export interface IUser {
   bio: string;
   profilePic: string;
 }
+
+export type TUserRegister = Pick<IUser, 'username' | 'email' | 'password'>;
+export type TUserLogin = Pick<IUser, 'email' | 'password'>;
