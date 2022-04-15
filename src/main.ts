c35d643d01e68ccea1c31f8a930b421597fc2ac3
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-04-14 18:16:14 -0600

    add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /**
+   * Swagger configuration
+   */
+  const config = new DocumentBuilder()
+    .setTitle('Medium Size Docs')
+    .setDescription('Doc for Medium Size API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  /**
+   * Start of the application
+   */
+  await app.listen(4000, () => {
+    console.log('Running on port 4000');
+  });
 }
 bootstrap();
