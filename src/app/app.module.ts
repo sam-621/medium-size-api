27515652d09c2dbe231324040d88c693a6df97cf
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-04-15 11:44:07 -0600

    integrate databse module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../common/database/mongo.module';
 import { ConfigModule } from '/@/config/config.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, DatabaseModule],
 })
 export class AppModule {}
