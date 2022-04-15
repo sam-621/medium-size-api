8a0cd06fda444b24dc3f0eaab65e2c03329fbe53
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-04-15 13:19:56 -0600

    add auth service

---
+++ src/app/auth/auth.interfaces.ts
@@ -0,0 +1,5 @@
+import { TMongoId } from '/@/common/interfaces/utils.interface';
+
+export type TPayload = {
+  id: TMongoId;
+};
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserRepository } from '../user/repository/user.repository';
+import bcrypt from 'bcrypt';
+import { SALT } from '/@/common/config/constants.config';
+import { TPayload } from './auth.interfaces';
+
+@Injectable()
+export class AuthService {
+  constructor(private userRepository: UserRepository, private jwtService: JwtService) {}
+
+  async hashPassword(password: string) {
+    return await bcrypt.hash(password, SALT);
+  }
+
+  async passwordsMatch(password: string, hash: string) {
+    return await bcrypt.compare(password, hash);
+  }
+
+  async emailAlreadyExists(email: string): Promise<boolean> {
+    const user = await this.userRepository.findOneByEmail(email);
+
+    return Boolean(user);
+  }
+
+  createJWT(payload: TPayload): string {
+    return this.jwtService.sign(payload, { expiresIn: '3d' });
+  }
+
+  verifyJWT(token: string): TPayload {
+    return this.jwtService.verify<TPayload>(token);
+  }
+}
