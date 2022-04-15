d5c8b0349281351906e2ca5add67d4ec7784e989
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-04-15 11:30:29 -0600

    add mongo module

---
+++ src/common/database/mongo.module.ts
@@ -0,0 +1,19 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+
+@Global()
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_DB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [MongooseModule],
+})
+export class DatabaseModule {}
