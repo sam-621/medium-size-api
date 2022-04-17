e255a7259428f2115a4ad1a62d22d8a40cfc7849
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-04-16 23:19:32 -0600

    add UserAuthController

--- src/app/auth/interfaces/auth.interfaces.ts
+++
@@ -1,5 +0,0 @@
-import { Types } from 'mongoose';
-
-export type TPayload = {
-  id: Types.ObjectId;
-};
--- src/app/user/controllers/userAuth.controller.ts
+++ src/app/user/controllers/userAuth.controller.ts
@@ -6,16 +6,16 @@ import {
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
-import { LoginDto } from '../../user/dtos/login.dto';
+import { LoginDto } from '../dtos/login.dto';
 import { RegisterDto } from '../dtos/register.dto';
-import { AuthResponse } from '../dtos/response.dto';
-import { AuthService } from '../services/auth.service';
-import { ErrorHttpResponse } from '/@/common/interfaces/responses.interface';
+import { UserAuthResponse } from '../dtos/response.dto';
+import { UserAuthService } from '../services/userAuth.service';
+import { ErrorHttpResponse } from '/@/interfaces/responses.interface';
 
-@ApiTags('Auth')
-@Controller('auth')
-export class AuthController {
-  constructor(private authService: AuthService) {}
+@ApiTags('User auth')
+@Controller('user/auth')
+export class UserAuthController {
+  constructor(private userAuthService: UserAuthService) {}
 
   @HttpCode(HttpStatus.CREATED)
   @Post('register')
@@ -23,11 +23,11 @@ export class AuthController {
     description: 'User already exists with that email',
     type: ErrorHttpResponse,
   })
-  @ApiCreatedResponse({ description: 'user created', type: AuthResponse })
+  @ApiCreatedResponse({ description: 'user created', type: UserAuthResponse })
   async register(@Body() user: RegisterDto) {
-    const token = await this.authService.register(user);
+    const token = await this.userAuthService.register(user);
 
-    return new AuthResponse(token);
+    return new UserAuthResponse(token);
   }
 
   @HttpCode(HttpStatus.OK)
@@ -35,11 +35,11 @@ export class AuthController {
   @ApiUnauthorizedResponse({ description: 'Wrong credentials', type: ErrorHttpResponse })
   @ApiOkResponse({
     description: 'user logged successfully',
-    type: AuthResponse,
+    type: UserAuthResponse,
   })
   async login(@Body() user: LoginDto) {
-    const token = await this.authService.login(user);
+    const token = await this.userAuthService.login(user);
 
-    return new AuthResponse(token);
+    return new UserAuthResponse(token);
   }
 }
