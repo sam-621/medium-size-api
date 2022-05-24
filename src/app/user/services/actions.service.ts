d91007a51e01e2d897aac28f51480b3f740251ef
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2022-05-23 23:25:29 -0600

    add follow service

--- src/app/user/services/actions.service.ts
+++ src/app/user/services/actions.service.ts
@@ -1,5 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { Types } from 'mongoose';
+import { UserRepository } from '../repository/user.repository';
 
 @Injectable()
-export class ActionsService {}
+export class ActionsService {
+  constructor(private userRepository: UserRepository) {}
+
+  async follow(userId: Types.ObjectId, followerId: Types.ObjectId) {
+    try {
+      const newUser = await this.userRepository.addFollower(userId, followerId);
+
+      return newUser;
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
+}
